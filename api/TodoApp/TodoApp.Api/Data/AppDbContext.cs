3d418ee1ee20e8602d9f7a75d2e49642a76953dc
Allan Salgado <allan.salgado3@gmail.com>
2024-11-04 18:16:52 -0600

    Add Entity Framework Core setup with TodoItem model and AppDbContext

---
+++ api/TodoApp/TodoApp.Api/Data/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Api.Models;
+
+namespace TodoApp.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TodoItem>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Title)
+                      .IsRequired()
+                      .HasMaxLength(200);
+
+                entity.Property(e => e.Description)
+                      .HasMaxLength(1000);
+
+                entity.Property(e => e.IsCompleted)
+                      .HasDefaultValue(false);
+
+                entity.Property(e => e.CreatedAt)
+                      .IsRequired()
+                      .HasDefaultValueSql("CURRENT_TIMESTAMP");
+            });
+        }
+    }
+}
---
+++ api/TodoApp/TodoApp.Api/Models/TodoItem.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TodoApp.Api.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [MaxLength(200)]
+        public required string Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public bool IsCompleted { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+
+}

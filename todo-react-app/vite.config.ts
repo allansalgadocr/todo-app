a1b6f613a6557d309e67de0187d00623e50a2317
Allan Salgado <allan.salgado3@gmail.com>
2024-11-06 19:11:07 -0600

    Refactor tailwind.config.js, tsconfig.app.json, and vite.config.ts

--- todo-react-app/vite.config.ts
+++ todo-react-app/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@services": path.resolve(__dirname, "src/services"),
+      "@components": path.resolve(__dirname, "src/components"),
+    },
+  },
+  server: {
+    host: true, // Expose the server on all network interfaces
+    port: 80,
+  },
+});
